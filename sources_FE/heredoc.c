13f6558b6e39f3098f2399b71b5bca254cf143e0
Ilia Kiriushkin <ikiriush@c1r2s3.42yerevan.am>
2025-12-11 03:27:22 +0400

    Add signal waiter

--- sources_FE/heredoc.c
+++ sources_FE/heredoc.c
@@ -6,28 +6,42 @@
 /*   By: ikiriush <ikiriush@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 22:54:05 by ikiriush          #+#    #+#             */
-/*   Updated: 2025/12/09 01:40:07 by ikiriush         ###   ########.fr       */
+/*   Updated: 2025/12/11 03:26:17 by ikiriush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	temp_file_writer(t_redir *rd, t_shell *sh)
+void	signal_waiter(t_shell *sh)
 {
-	int		wfd;
+	int		status;
+	int		last_status;
+
+	last_status = 0;
+	while (wait(&status) > 0)
+	{
+		if (WIFEXITED(status))
+			last_status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			last_status = 128 + WTERMSIG(status);
+		sh->exit_code = last_status;
+	}
+}
+
+static void	heredoc_read_child(int wfd, t_redir *rd, t_shell *sh)
+{
 	char	*line;
 
-	wfd = open(".tmp", O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (wfd == -1)
-		fatal_error("open", sh);
 	while (42)
 	{
 		line = readline(">");
-		if (!line || !ft_strcmp(rd->target, line))
+		if (!line)
 		{
-			free(line);
+			syntax_errorer_heredoc(rd, sh);
 			break ;
 		}
+		else if (!ft_strcmp(rd->target, line))
+			break ;
 		if (ft_strchr(line, '$') && rd->do_expand)
 			heredoc_expander(&line, sh);
 		if (write(wfd, line, ft_strlen(line)) == -1)
@@ -36,6 +50,33 @@ static int	temp_file_writer(t_redir *rd, t_shell *sh)
 			fatal_error("write (2)", sh);
 		free(line);
 	}
+	close(wfd);
+	safe_free(line);
+	free_front_end_shell(sh);
+	free_env(sh);
+	exit(0);
+}
+
+static int	temp_file_writer(t_redir *rd, t_shell *sh)
+{
+	int		wfd;
+	pid_t	pid;
+
+	wfd = open(".tmp", O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (wfd == -1)
+		fatal_error("open", sh);
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	pid = fork();
+	if (pid == -1)
+		fatal_error("fork", sh);
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		heredoc_read_child(wfd, rd, sh);
+	}
+	signal_waiter(sh);
+	signal(SIGINT, sigint_handler);
 	return (wfd);
 }
 
@@ -45,12 +86,12 @@ static int	heredoc_writer(t_redir *rd, t_shell *sh)
 	int		rfd;
 
 	wfd = temp_file_writer(rd, sh);
-	if (close(wfd) == -1)
-		fatal_error("close", sh);
-	wfd = -1;
 	rfd = open(".tmp", O_RDONLY, 0600);
 	if (rfd == -1)
 		fatal_error("open", sh);
+	if (close(wfd) == -1)
+		fatal_error("close", sh);
+	wfd = -1;
 	unlink(".tmp");
 	return (rfd);
 }
